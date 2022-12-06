4a416685043cfab3c708dc5c642922bcb4148a5b
riemogerz <rizqanhukum@gmail.com>
2022-12-06 14:03:36 +0700

    Menambahkan data dapil dan perolehan suara

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const datasRoutes = require('./datasRoutes')
+const pemiluRoutes = require('./pemiluRoutes')
 const errorHandler = require('../middlewares/errorHandler')
 
 router.use('/datas', datasRoutes)
+
+router.use('/pemilu', pemiluRoutes)
 
 router.use(errorHandler)
 module.exports = router
